9432878bdc3f197d16a25542e199981e28559aa6
The Peregrn Protocol <tsatoshiearn@gmail.com>
2025-07-08 10:37:10 +0200

    Add files via upload

--- modules/terminal-fx.js
+++ modules/terminal-fx.js
@@ -1,1 +1,13 @@
-
+class TerminalFX {
+  static typewriter(element, text, speed = 50) {
+    let i = 0;
+    const typing = setInterval(() => {
+      if (i < text.length) {
+        element.textContent += text.charAt(i);
+        i++;
+      } else {
+        clearInterval(typing);
+      }
+    }, speed);
+  }
+}
